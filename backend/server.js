2aa7df70ea1095fbb29d54dd416a50b055af20b3
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2025-11-13 19:55:00 +0600

    Database Connection setup locally

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",    
+  password: "",       
+  database: "stock_market",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("DB connection failed:", err);
+    return;
+  }
+  console.log("DB connection successfull");
+});
+
+
+app.get("/api/stocks", (req, res) => {
+  const sql = "SELECT * FROM stocks";  
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error running query:", err);
+      return res.status(500).json({ error: "Database query failed" });
+    }
+    res.json(results);
+  });
+});
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
